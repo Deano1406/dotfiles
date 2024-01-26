fe77c688cd4ece8cabd0ae0038d0fb2ae151d169
Dean Bevan <dean.bevan@phoenix-web.org.uk>
2024-01-26 13:13:49 +0000

    Added msason-tool-installer to handle the automatic load of formatters and other tools

--- dot_config/nvim/lua/empty_lsp-config.lua
+++ dot_config/nvim/lua/empty_lsp-config.lua
@@ -1,6 +1,8 @@
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require("lspconfig")
+local conform = require("conform")
+local mason_tool_installer = require("mason-tool-installer")
 
 mason.setup()
 mason_lspconfig.setup({
@@ -9,6 +11,7 @@ mason_lspconfig.setup({
 		"html",
 		"pyright",
 		"lua_ls",
+		"yamlls",
 	},
 	automatic_installation = true,
 })
@@ -17,3 +20,41 @@ lspconfig.lua_ls.setup({})
 lspconfig.pyright.setup({})
 lspconfig.html.setup({})
 lspconfig.tsserver.setup({})
+lspconfig.yamlls.setup({})
+
+mason_tool_installer.setup({
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"isort",
+		"black",
+	},
+	auto_update = true,
+	debounce_hours = 3,
+})
+
+conform.event = { "BufReadPre", "BufNewFile" }
+conform.setup({
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		svelte = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		graphql = { "prettier" },
+		lua = { "stylua" },
+		python = { "isort", "black" },
+	},
+	vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		conform.format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 5000,
+		})
+	end, { desc = "Format file or range (in visual mode)" }),
+})
--- dot_config/nvim/lua/plugins/empty_mason.lua
+++ dot_config/nvim/lua/plugins/empty_mason.lua
@@ -3,5 +3,7 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
+		"stevearc/conform.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 }
